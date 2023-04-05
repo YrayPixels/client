f56a83ebe033fe97cca83b0147606ed8c3ddb993
YrayPixels <moseserhinyodavwe2@gmail.com>
2023-04-05 19:15:45 +0100

    gh

--- resources/views/mail/email/processingOrder.blade.php
+++ resources/views/mail/email/processingOrder.blade.php
@@ -1,17 +1,28 @@
-<x-mail::message>
+<div class="email-container"
+    style="max-width: 50%; min-width: 300px; height: 80vh; background-color: #edf2f6; margin: 1rem auto; padding-top: 1rem; border-radius: 10px;">
+    <h1 style="text-align:center;">GAPA</h1>
+    <div style="display: flex; justify-content: center;">
+        <div style="margin: auto 1rem;">
+            <div>
+                <p>
+                    Thanks for creating an account with GAPA Naija. Your registered email address is
+                    <span style="font-weight: bold"></span>. You can access your account area to view orders,
+                    change your password and more at <a href="gapaautoparts.com/home">https://gapaautoparts.com/home</a>
+                </p>
+            </div>
+            <p>We look forward to seeing you soon and also purchasing from us.</p>
+            <p>For any question, kindly check our Faq or send a mail to <a href="mailto::contact@gapaautoparts.com">
+                    contact@gapaautoparts.com</a> or you can chat with our live chat directly on the website</p>
 
-<div><p>
-    Thanks for creating an account with GAPA Naija. Your registered email address is 
-    <span style="font-weight: bold"></span>. You can access your account area to view orders,
-    change your password and more at <a href="gapaautoparts.com/home">https://gapaautoparts.com/home</a>
-     </p>
-    </div><p>We look forward to seeing you soon and also purchasing from us.</p>
-    <p>For any question, kindly check our Faq or send a mail to <a href="mailto::contact@gapaautoparts.com">
-        contact@gapaautoparts.com</a> or you can chat with our live chat directly on the website</p>
-   <i>the details are shown below</i>
-      <div>
-     
+            <div>
+            </div>
+            <div style="margin-top: 3rem;">
+                <p>Regard</p>
+                <p style="font-size: 1.1em;">GapaAuto</p>
+            </div>
+        </div>
 
-Thanks,<br>
-{{ config('app.name') }}
-</x-mail::message>
+
+    </div>
+
+</div>
